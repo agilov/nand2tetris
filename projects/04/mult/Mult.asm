cef83cbfb68d76b4fd38e542853b69840a4df557
agilov <agilovr@gmail.com>
2022-12-27 10:43:26 +0300

    Created mult from chapter 4

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+(SETUP)
+    // Set product to 0
+    @R2
+    M=0
+
+    // If first operand is 0 result is 0
+    @R0
+    D=M
+    @END
+    D;JEQ
+
+    // If second operand is 0 result is 0
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+// Multiplication loop
+(LOOP)
+    // Load current product to the regiser D
+    @R2
+    D=M
+
+    @R0 // Load first operand to the register A
+    D=D+M // Add first operand and product
+
+    @R2
+    M=D // Save product to memory (@R2) from register D
+
+    // Decrement second operand
+    @R1
+    D=M-1
+    M=D
+    @LOOP // Repeat if second repeat is not 0
+    D;JGT
+(END)
+    @END
+    0;JMP
